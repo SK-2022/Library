ba35370be327f410de7f90d0bd6c883ba93d8443
Mika Chiusiwa <mikachiusiwa@gmail.com>
2023-09-06 08:44:54 +0900

    Add icons and images and center them accordingly

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-let myLibrary = []
-//The book constructor function
-function Book(title, author, pages, readingStatus){
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.readingStatus = readingStatus
+// let myLibrary = []
+// //The book constructor function
+// function Book(title, author, pages, readingStatus){
+//     this.title = title
+//     this.author = author
+//     this.pages = pages
+//     this.readingStatus = readingStatus
     
-    }
+//     }
 
-function addBookToLibrary(book){
-    myLibrary.push(book)
-}
+// function addBookToLibrary(book){
+//     myLibrary.push(book)
+// }
 
-const theWitcher = new Book('The Witcher: The last wish','Andrzej Sapkowski', '288 pages', 'not read' )
+// const theWitcher = new Book('The Witcher: The last wish','Andrzej Sapkowski', '288 pages', 'not read' )
 
-const deepWork = new Book('Deep Work', ' Cal Newport', ' 287 pages', ' read')
+// const deepWork = new Book('Deep Work', ' Cal Newport', ' 287 pages', ' read')
 
-addBookToLibrary(theWitcher)
-addBookToLibrary(deepWork)
+// addBookToLibrary(theWitcher)
+// addBookToLibrary(deepWork)
 
-let bookCard = document.querySelector('.book-card')
-bookCard.textContent = Object.values(deepWork)
+// let bookCard = document.querySelector('.book-card')
+// bookCard.textContent = Object.values(deepWork)
