541c011f55c8b8cbcb35d11e861d8c4ddb51b6b1
Mika Chiusiwa <mikachiusiwa@gmail.com>
2023-09-06 09:57:24 +0900

    Add main book card section and basic logic for the app

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
-// let myLibrary = []
-// //The book constructor function
-// function Book(title, author, pages, readingStatus){
-//     this.title = title
-//     this.author = author
-//     this.pages = pages
-//     this.readingStatus = readingStatus
-    
-//     }
+const myLibrary = ['Witcher','Deep Work','Pragmatic programmer'];
 
-// function addBookToLibrary(book){
-//     myLibrary.push(book)
-// }
+//Constructor function for my books
+function Book(bookTitle, author, pageNumbers, status){
+    this.bookTitle = bookTitle;
+    this.author = author;
+    this.pageNumbers = pageNumbers
+    this.status = status
+}
 
-// const theWitcher = new Book('The Witcher: The last wish','Andrzej Sapkowski', '288 pages', 'not read' )
+//Function to add books to the library.
+function addBookToLibrary(){
 
-// const deepWork = new Book('Deep Work', ' Cal Newport', ' 287 pages', ' read')
-
-// addBookToLibrary(theWitcher)
-// addBookToLibrary(deepWork)
-
-// let bookCard = document.querySelector('.book-card')
-// bookCard.textContent = Object.values(deepWork)
+}
