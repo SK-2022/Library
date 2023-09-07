82ae13b6331e9b42de1547897bc6ae4af4d75fbb
Mika Chiusiwa <mikachiusiwa@gmail.com>
2023-09-07 09:48:00 +0900

    Add button functionality to the addBook button and style the modal dialog

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-const myLibrary = ['Witcher','Deep Work','Pragmatic programmer'];
+// const myLibrary = ['Witcher','Deep Work','Pragmatic programmer'];
 
-//Constructor function for my books
-function Book(bookTitle, author, pageNumbers, status){
-    this.bookTitle = bookTitle;
-    this.author = author;
-    this.pageNumbers = pageNumbers
-    this.status = status
-}
+// //Constructor function for my books
+// function Book(bookTitle, author, pageNumbers, status){
+//     this.bookTitle = bookTitle;
+//     this.author = author;
+//     this.pageNumbers = pageNumbers
+//     this.status = status
+// }
 
-//Function to add books to the library.
-function addBookToLibrary(){
+// //Function to add books to the library.
+// // function addBookToLibrary(){
 
-}
+// // }
+
+const addBookButton = document.querySelector('.add-book-button');
+const dialogBox = document.querySelector('.dialog-box');
+
+addBookButton.addEventListener('click', () => {
+    dialogBox.showModal();
+})
