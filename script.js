91148a48ca36c229427649cb3b8e3179f174e41a
Mika Chiusiwa <mikachiusiwa@gmail.com>
2023-09-12 08:46:36 +0900

    Add first bit of logic to the render function

--- script.js
+++ script.js
@@ -1,52 +1,65 @@
-
-// Book Card Reading status btn toggle logic
-const readingStatusBtn = document.querySelectorAll('.toggle-button')
-
-readingStatusBtn.forEach((button) => {
-    let isToggled = false; // Initial state is "Not toggled"
-
-    button.addEventListener('click', () => {
-
-        isToggled = !isToggled //Toggle the state making it true
-        button.classList.toggle('reading-status-btn');
-        button.classList.toggle('not-read');
-        
-        // Toggle the text based on the state
-        if(isToggled){
-            button.textContent = 'Not read'
-        } else{
-            button.textContent = 'Read'
-        }
-        })
-})
-
 // Remember, each element in your array is an object
-const myLibrary = [
-    {bookTitle: 'The Pragmatic programmer', author: 'Andy Hunt and Dave Thomas', pageNumbers: 352, status: 'read' }
-    //Insert more books here
-];
+const myLibrary = [];
 
 //Constructor function for my books
-function Book(bookTitle, author, pageNumbers, status){
+function Book(bookTitle, author, pageNumbers, readingStatus){
     this.bookTitle = bookTitle;
     this.author = author;
     this.pageNumbers = pageNumbers
-    this.status = status
+    this.readingStatus = readingStatus
+}
+
+//This adds the books to the page display
+function render() {
+    let libraryContainerElement = document.querySelector('.book-card-container')
+    myLibrary.forEach((i) => {
+        let book = myLibrary[i]
+        let bookCardElement = document.createElement('div')
+        bookCardElement.innerHTML = 
+        `<div class="book-card">
+        <div class="book-title">${book.bookTitle}</div>
+        <div class="book-author">By ${book.author}</div>
+        <div class="pages">Pages: ${book.pageNumbers}</div>
+        <div class="book-card-btn-container">
+            <button class="toggle-button reading-status-btn">${book.readingStatus}</button>
+            <img class="trash-icon" src="/images/trash-icon.png" alt="Remove button trashcan">
+        </div>
+        <div class="my-rating-container">
+            <label for="my-rating" class="book-rating">My Rating:</label>
+            <select name="my-rating" id="my-rating">
+                <option value="not sure ">Not sure </option>
+                <option value="terrible">Terrible</option>
+                <option value="bad">Bad</option>
+                <option value="ok">OK</option>
+                <option value="good">Good</option>
+                <option value="amazing">Amazing</option>
+            </select>
+        </div>
+    </div>`
+    libraryContainerElement.appendChild(bookCardElement)
+     })
+
+}
+
+//Add to library function
+function addBookToLibrary(){
+     //Selects all inputs from the dialog
+     const title = document.getElementById('title').value
+     const author = document.getElementById('author').value
+     const pages = document.getElementById('pages').value
+     const readingStatus = document.getElementById('reading-status').checked
+
+     //Creates new book object using my constructor function
+     let book = new Book(title, author, pages, readingStatus);
+     myLibrary.push(book);
+     render();
 }
 
 
-//Test Function for submit button to Add book to library
    const bookSubmitBtn = document.querySelector('.book-form')
- bookSubmitBtn.addEventListener('submit', function addBookToLibrary(event) {
+   bookSubmitBtn.addEventListener('submit', function (event) {
     event.preventDefault(); //Prevent the default form submission behavior
-    //Selects all inputs from the dialog
-    const title = document.getElementById('title').value
-    const author = document.getElementById('author').value
-    const pages = document.getElementById('pages').value
-    const readingStatus = document.getElementById('reading-status').checked
-    //Creates new book object using my constructor function
-    let book = new Book(title, author, pages, readingStatus)
-    console.log(book)
+    addBookToLibrary()
  })
 
 const addBookButton = document.querySelector('.add-book-button');
@@ -56,3 +69,45 @@ const dialogBox = document.querySelector('.dialog-box');
 addBookButton.addEventListener('click', () => {
     dialogBox.showModal();
 })
+
+
+// Book Card Reading status btn toggle logic
+const readingStatusBtn = document.querySelectorAll('.toggle-button')
+
+//This gets the job done but its inefficient. Use a prototype.
+// readingStatusBtn.forEach((button) => {
+//     let isToggled = false; // Initial state is "Not toggled"
+
+//     button.addEventListener('click', () => {
+
+//         isToggled = !isToggled //Toggle the state making it true
+//         button.classList.toggle('reading-status-btn');
+//         button.classList.toggle('not-read');
+        
+//         // Toggle the text based on the state
+//         if(isToggled){
+//             button.textContent = 'Not read'
+//         } else{
+//             button.textContent = 'Read'
+//         }
+//         })
+// })
+
+//More Efficient
+ Book.prototype.toggleRead = function (readingStatusBtn){
+    let isToggled = false; // Initial state is "Not toggled"
+
+    readingStatusBtn.addEventListener('click', () => {
+
+        isToggled = !isToggled //Toggle the state making it true
+        readingStatusBtn.classList.toggle('reading-status-btn');
+        readingStatusBtn.classList.toggle('not-read');
+        
+        // Toggle the text based on the state
+        if(isToggled){
+            readingStatusBtn.textContent = 'Not read'
+        } else{
+            readingStatusBtn.textContent = 'Read'
+        }
+        })
+}
